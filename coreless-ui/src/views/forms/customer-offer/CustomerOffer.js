92082a51dc9290512db07dc7733c0e44bf74f60f
Ilona Shishov <Ilona.Shishov@gmail.com>
2021-11-23 12:59:54 +0200

    add coreless-ui
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>

--- coreless-ui/src/views/forms/customer-offer/CustomerOffer.js
+++ coreless-ui/src/views/forms/customer-offer/CustomerOffer.js
@@ -19,16 +19,46 @@ export default class CustomerOffer extends React.Component {
 
     this.state = {
       customerFirstName: '',
+      customerLastName: '',
     }
   }
   handleInputChanged(event) {
-    this.setState({
-      customerFirstName: event.target.value,
-    })
+    switch (event.target.id) {
+      case 'CustomerFirstName':
+        this.setState({
+          customerFirstName: event.target.value,
+        })
+        break
+      case 'CustomerLastName':
+        this.setState({
+          customerLastName: event.target.value,
+        })
+        break
+    }
   }
   handleButtonClicked() {
-    window.location.href =
-      'https://youtube.com/results?search_query=' + this.state.customerFirstName
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ procedure: { customerReference: this.state.customerFirstName } }),
+    }
+    fetch('/co', requestOptions).then((res) => console.log(res.json()))
+    // fetch('/api').then((res) => console.log(res.json()))
+    // const requestOptions = {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({ procedure: { customerReference: this.state.customerFirstName } }),
+    // }
+    // fetch(
+    //   'https://192.168.49.2:31951/customer-offer/sd1/customer-offer-procedure/initiation',
+    //   requestOptions,
+    // ).then((response) => response.json())
+    // console.log('post executed')
+    // window.location.href =
+    // 'https://youtube.com/results?search_query=' +
+    // this.state.customerFirstName +
+    // ' ' +
+    // this.state.customerLastName
   }
 
   render() {
@@ -51,12 +81,18 @@ export default class CustomerOffer extends React.Component {
                 />
               </div>
               <div className="mb-3">
-                <CFormLabel htmlFor="CustomerEmail">Email address</CFormLabel>
-                <CFormInput type="email" id="CustomerEmail" placeholder="name@example.com" />
+                <CFormLabel htmlFor="CustomerLastName">Last Name</CFormLabel>
+                <CFormInput
+                  type="text"
+                  id="CustomerLastName"
+                  placeholder="Doe"
+                  value={this.state.customerLastName}
+                  onChange={this.handleInputChanged.bind(this)}
+                />
               </div>
               <div className="mb-3">
-                <CFormLabel htmlFor="CustomerLastName">Last Name</CFormLabel>
-                <CFormInput type="text" id="CustomerLastName" placeholder="Doe" />
+                <CFormLabel htmlFor="CustomerEmail">Email address</CFormLabel>
+                <CFormInput type="email" id="CustomerEmail" placeholder="name@example.com" />
               </div>
               <div className="mb-3">
                 <CFormLabel htmlFor="CustomerAddress">Address</CFormLabel>
